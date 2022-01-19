2f2f2ddebdbf40b5aac04ee6928f23744b49de7b
sub <spoon718@gmail.com>
2022-01-19 13:06:28 +0900

    asf

--- src/components/input/index.jsx
+++ src/components/input/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styled from "styled-components";
 
-const Input = () => {
-  return <input type="text" />
+
+const InputTxt = styled.input`
+  padding: 5px;
+  border: 1px solid #ddd;
+`
+
+
+const Input = (props) => {
+  return <InputTxt  {...props} />
 }
 
 export default Input
